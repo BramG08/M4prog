75ff294b968d35a5f7c36b708c38ad12eb025532
BramG08 <bgrippeling08@gmail.com>
2025-05-15 12:23:35 +0200

    complexselect

---
+++ 01 opstarten/dockerM4Prog/sql/03/complexselect.sql
@@ -0,0 +1,4 @@
+SELECT * FROM `Applicatie` ORDER BY ScreenTime;
+SELECT * FROM `Applicatie` ORDER BY ScreenTime LIMIT 2;
+SELECT SUM(ScreenTime)FROM `Applicatie`;
+SELECT SUM(ScreenTime)FROM `Applicatie` ORDER by ScreenTime ASC LIMIT 3;
