3a7b1cd49182be5165724de744d35da678a721c4
BramG08 <bgrippeling08@gmail.com>
2025-05-15 11:47:06 +0200

    Create rekenen

---
+++ 01 opstarten/dockerM4Prog/sql/03/rekenen.sql
@@ -0,0 +1,4 @@
+SELECT sum(leeftijd) FROM `huisdieren`;
+SELECT avg(leeftijd) FROM `huisdieren`;
+SELECT avg(leeftijd), sum(leeftijd) FROM `huisdieren`;
+SELECT sum(leeftijd)as gemiddeldeLeeftijd FROM`huisdieren`;
