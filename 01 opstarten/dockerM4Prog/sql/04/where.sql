091bdc422f913218592ec465a6f2f8c7d99ca565
BramG08 <bgrippeling08@gmail.com>
2025-05-19 14:04:39 +0200

    Where.sql

---
+++ 01 opstarten/dockerM4Prog/sql/04/where.sql
@@ -0,0 +1,4 @@
+SELECT * from `huisdieren` WHERE Leeftijd <= 9;
+SELECT * from `huisdieren` WHERE Leeftijd > 10;
+SELECT * from `huisdieren` WHERE naam = 'Flip';
+SELECT * FROM `huisdieren` WHERE type = 'Draak';
